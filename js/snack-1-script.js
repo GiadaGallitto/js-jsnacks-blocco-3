288a76c18cdfc8b448717b479a4d3d3668236767
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-28 16:35:13 +0100

    Add do while loop

--- js/snack-1-script.js
+++ js/snack-1-script.js
@@ -1,8 +1,10 @@
 // Continuare a chiedere all'utente un numero tra 1 e 10 
 // fino a quando l'utente non inserisce il valore corretto
 
-let userNumber = parseInt(prompt(`Inserisci un numero tra 1 e 10`));
+let userNumber;
 
-while(userNumber > 10 || userNumber < 1 ){
+do{
     userNumber = parseInt(prompt(`Inserisci un numero tra 1 e 10`));
+} while(userNumber > 10 || userNumber < 1 ) {
+
 }

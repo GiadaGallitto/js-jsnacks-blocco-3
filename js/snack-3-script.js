f14269d49107d360226cf5313d6c6924e1416bd8
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-28 17:31:13 +0100

    Fix code and Add random Number

--- js/snack-3-script.js
+++ js/snack-3-script.js
@@ -5,10 +5,12 @@ const shortList = [1, 2, 3, 4, 5, 6]
 
 const longList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-let randomElement = 1;
+let randomElement;
 
-while(shortList.length < longList.length){
+while(shortList.length != longList.length){
+    randomElement = Math.floor(Math.random() * longList.length);
     shortList.push(randomElement)
 }
 
+console.log(randomElement);
 console.log(shortList, longList);

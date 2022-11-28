d2a99c3be3025d383e511dcf6fd3ad0e01ee1dc1
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-28 17:33:52 +0100

    Fix code

--- js/snack-3-script.js
+++ js/snack-3-script.js
@@ -1,15 +1,16 @@
 // Crea due array che hanno un numero di elementi diversi. 
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
-const shortList = [1, 2, 3, 4, 5, 6]
+const shortList = [1, 2, 3, 4, 5, 6];
 
-const longList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const longList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let randomElement;
 
 while(shortList.length != longList.length){
     randomElement = Math.floor(Math.random() * longList.length);
-    shortList.push(randomElement)
+    shortList.push(randomElement);
+    console.log(randomElement);
 }
 
 console.log(randomElement);
